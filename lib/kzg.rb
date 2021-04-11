f29421abdd814aaf5c2e724f1b15bd7ccb62135d
azuchi <azuchi@chaintope.com>
2021-04-11 15:01:59 +0900

    initial commit

---
+++ lib/kzg.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "kzg/version"
+
+module KZG
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/kzg/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KZG
+  VERSION = "0.1.0"
+end
