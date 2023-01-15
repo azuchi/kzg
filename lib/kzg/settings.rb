c245f5bb586872894b92ea42f563d6edfcb6eb70
azuchi <azuchi@chaintope.com>
2023-01-15 16:05:01 +0900

    Refactoring rename variables

--- lib/kzg/settings.rb
+++ lib/kzg/settings.rb
@@ -9,9 +9,9 @@ module KZG
     # @param [Array[BLS::PointG1]] g1s
     # @param [Array[BLS::PointG2]] g2s
     def initialize(g1_points, g2_points)
-      raise KZG::Error, 'g1s and g2s must be array.' if !g1_points.is_a?(Array) || !g2_points.is_a?(Array)
-      raise KZG::Error, 'All elements of g1s must be BLS::PointG1.' unless g1_points.all? { |g| g.is_a?(BLS::PointG1) }
-      raise KZG::Error, 'All elements of g2s must be BLS::PointG2.' unless g2_points.all? { |g| g.is_a?(BLS::PointG2) }
+      raise KZG::Error, 'g1_points and g2_points must be array.' if !g1_points.is_a?(Array) || !g2_points.is_a?(Array)
+      raise KZG::Error, 'All elements of g1_points must be BLS::PointG1.' unless g1_points.all? { |g| g.is_a?(BLS::PointG1) }
+      raise KZG::Error, 'All elements of g2_points must be BLS::PointG2.' unless g2_points.all? { |g| g.is_a?(BLS::PointG2) }
 
       @g1_points = g1_points
       @g2_points = g2_points
