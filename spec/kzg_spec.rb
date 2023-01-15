04c82099ef0e7228f3633b99426303e29035f044
azuchi <azuchi@chaintope.com>
2023-01-15 15:51:20 +0900

    Fix variable name

--- spec/kzg_spec.rb
+++ spec/kzg_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe KZG do
   describe "#setup" do
     it do
       secret = 11
-      degree = 16
-      setup1 = KZG.setup_params(secret, degree)
-      setup2 = KZG.setup_params(secret, degree)
+      n = 16
+      setup1 = KZG.setup_params(secret, n)
+      setup2 = KZG.setup_params(secret, n)
       expect(setup1).to eq(setup2)
+      expect(setup1.g1s.length).to eq(n)
     end
   end
 end
